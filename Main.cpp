7b5f366ab195d0e61c0068e10d6e379885a0a751
ai-dev-official <ai.codespro@gmail.com>
2022-11-05 17:35:53 +0000

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string Managers;
+	string HrlyPaidWorkers;
+	string SalesStaff;
+	string ProductionLineStaff;
+
+	int paycode;
+	double fixedHrlySalary=10.0;
+	double fixedWklySalary=200.0;
+	double hrsWorked;
+	double daysWorked;
+	double weeksWorked;
+	double salary;
+	double extraHours;
+	double grossWklySales;
+	int numItemsProduced;
+	double amountPerItemProduced=20.0;
+	
+	std::cout << "Enter your paycode to get your salary or -1 to Quit the program" << endl;
+	std::cin >> paycode;
+
+	do
+	{
+		switch (paycode)
+		{
+		case 1:
+			std::cout << "You are a Manager" << endl;
+			std::cout << "Enter the number of weeks you worked: " << endl;
+			std::cin >> weeksWorked;
+			salary = weeksWorked * fixedWklySalary;
+			std::cout << "Your salary is: " << salary << endl;
+			break;
+
+		case 2:
+			std::cout << "You are an Hourly paid worker"  << endl;
+			std::cout << "Enter the number of hours you worked: " << endl;
+			std::cin >> hrsWorked;
+			std::cout << "Enter the number of extra hours you worked: " << endl;
+			std::cin >> extraHours;
+			salary = (fixedHrlySalary * hrsWorked) + (extraHours * 1.5);
+			std::cout << "Your salary is: " << salary << endl;
+			break;
+
+		case 3: 
+			std::cout << "You are a Sales Staff" << endl;
+			std::cout << "Enter the number of days you worked: " << endl;
+			std::cin >> daysWorked;
+			std::cout << "Enter the amount of Gross Weekly sales you made: " << endl;
+			std::cin >> grossWklySales;
+			salary = (daysWorked * 250) + (grossWklySales * 5.7 / 100);
+			std::cout << "Your salary is: " << salary << endl;
+			break;
+
+		case 4:
+			std::cout << "You are a Production Line Staff" << endl;
+			std::cout << "Enter the number Items of you produced: " << endl;
+			std::cin >> numItemsProduced;
+			salary = numItemsProduced * amountPerItemProduced;
+			std::cout << "Your salary is: " << salary << endl;
+			break;
+
+		default:
+			if (paycode < -1 || paycode > 4)
+			{
+				std::cout << "Error: Invalid Paycode" << endl;
+			}
+
+			break;
+		}
+
+		std::cout << "Enter your paycode to get your salary or -1 to Quit the program" << endl;
+		std::cin >> paycode;
+
+	} while (paycode != -1);
+
+
+	return 0;
+}
